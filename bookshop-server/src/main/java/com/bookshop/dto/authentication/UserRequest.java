a73f02ed473343d7ac6cf2e3598c6bac104d67d4
tutungduong <tu.tungduong@outlook.com>
2024-08-21 20:47:40 +0700

    add DTO User Request and Response

--- bookshop-server/src/main/java/com/bookshop/dto/authentication/UserRequest.java
+++ bookshop-server/src/main/java/com/bookshop/dto/authentication/UserRequest.java
@@ -1,4 +1,12 @@
 package com.bookshop.dto.authentication;
 
+
+import lombok.Data;
+
+@Data
 public class UserRequest {
+    private String username;
+    private String password;
+    private String gmail;
+
 }
--- bookshop-server/src/main/java/com/bookshop/dto/authentication/UserResponse.java
+++ bookshop-server/src/main/java/com/bookshop/dto/authentication/UserResponse.java
@@ -1,4 +1,16 @@
 package com.bookshop.dto.authentication;
 
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
 public class UserResponse {
+    private Long id;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String username;
+    private String password;
+    private String gmail;
 }
