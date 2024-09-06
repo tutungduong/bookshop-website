d83417178bbb6c0d12af504f4dfd603f157a8fe6
Tu Tung Duong <tu.tungduong@outlook.com>
2024-09-06 13:05:05 +0700

    add Order Controller for Manager

---
+++ bookshop-server/src/main/java/com/bookshop/controller/order/OrderController.java
@@ -0,0 +1,24 @@
+package com.bookshop.controller.order;
+
+import com.bookshop.service.order.OrderService;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+@AllArgsConstructor
+public class OrderController {
+
+    private OrderService orderService;
+
+    @PutMapping("/cancel/{code}")
+    public ResponseEntity<ObjectNode> cancelOrder(@PathVariable String code) {
+        orderService.cancelOrder(code);
+        return ResponseEntity.status(HttpStatus.OK).body(new ObjectNode(JsonNodeFactory.instance));
+    }
+
+}
